f558195875336668f1c1ff0208925ec6d622303f
venkatesh <venkatesh@server1.example.local>
2019-06-06 13:54:09 +0530

    More about detail

--- projects/src/portal/urls.py
+++ projects/src/portal/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from .views import home
+from blog.views import post_model_list_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/$', admin.site.urls),
+    url(r'^$', home, name='home'),
+    url(r'^blog/$', include('blog.urls', namespace='blog')),
 ]
